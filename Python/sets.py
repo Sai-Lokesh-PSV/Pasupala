43779e51c99ab95645ce82daa1bb6709745e4a12
Sai-Lokesh-PSV <78161763+Sai-Lokesh-PSV@users.noreply.github.com>
2021-02-01 12:40:48 +0530

    made sets.py user friendly

---
+++ Python/sets.py
@@ -0,0 +1,11 @@
+# Create an empty set
+s = set()
+
+#adding elements to a set
+hello = input("A number:")
+hi = input("An another number:")
+bye = input("An another number:")
+s.add(hello)
+s.add(hi)
+s.add(bye)
+print("here is your set",s)
